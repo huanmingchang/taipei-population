3fa699b10cef8d9551d80c1b291a7d1a104de326
huanmingchang <huanming1988@gmail.com>
2022-05-04 20:39:57 +0800

    refactor: refactor code due to react v.17

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
