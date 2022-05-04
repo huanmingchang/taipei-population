47a49706d5739febe5b4302d5f224c0eb879362b
huanmingchang <huanming1988@gmail.com>
2022-05-04 20:41:40 +0800

    feat: finish bar chart

--- src/pages/Home/components/Chart.js
+++ src/pages/Home/components/Chart.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import {
   BarChart,
   Bar,
@@ -11,9 +10,9 @@ import {
 
 const Chart = ({ population }) => {
   let data = []
-  console.log(population[0])
-
-  function fetchProps() {
+  if (population.length === 0) {
+    data = []
+  } else {
     data = [
       {
         name: '共同生活戶',
@@ -26,58 +25,56 @@ const Chart = ({ population }) => {
         female: population[0].single_f,
       },
     ]
-
-    return data
   }
 
-  useEffect(() => {
-    fetchProps()
-  }, [population])
-
   return (
-    <div className='px-10 pt-10 flex-grow w-full border-4 border-solid'>
-      <ResponsiveContainer width='100%' height='100%'>
-        <BarChart height='700px' data={data}>
-          <CartesianGrid opacity={0.5} vertical={false} />
-          <XAxis dataKey='name' axisLine={false} tickLine={false} />
-          <YAxis
-            axisLine={false}
-            tickLine={false}
-            tickCount={10}
-            ticks={[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
-            domain={[0, 100]}
-          />
-          <Legend
-            formatter={(value) => {
-              if (value.includes('female')) {
-                return (value = '女')
-              } else {
-                return (value = '男')
-              }
-            }}
-          />
-          <Bar
-            label={{
-              fill: 'rgb(159 18 57)',
-              fontSize: 20,
-              fontWeight: 700,
-              position: 'top',
-            }}
-            dataKey='male'
-            fill='rgb(100 116 139)'
-          />
-          <Bar
-            label={{
-              fill: 'rgb(159 18 57)',
-              fontSize: 20,
-              fontWeight: 700,
-              position: 'top',
-            }}
-            dataKey='female'
-            fill='rgb(244 114 182)'
-          />
-        </BarChart>
-      </ResponsiveContainer>
+    <div className='px-10 pt-10 flex-grow w-full h-full'>
+      {data.length === 0 ? (
+        <div>資料載入中請稍候</div>
+      ) : (
+        <ResponsiveContainer width='100%' height='100%'>
+          <BarChart width='100%' height='100%' data={data}>
+            <CartesianGrid opacity={0.5} vertical={false} />
+            <XAxis dataKey='name' axisLine={false} tickLine={false} />
+            <YAxis
+              axisLine={false}
+              tickLine={false}
+              tickCount={10}
+              ticks={[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+              domain={[0, 100]}
+            />
+            <Legend
+              formatter={(value) => {
+                if (value.includes('female')) {
+                  return (value = '女')
+                } else {
+                  return (value = '男')
+                }
+              }}
+            />
+            <Bar
+              label={{
+                fill: 'rgb(159 18 57)',
+                fontSize: 20,
+                fontWeight: 700,
+                position: 'top',
+              }}
+              dataKey='male'
+              fill='rgb(100 116 139)'
+            />
+            <Bar
+              label={{
+                fill: 'rgb(159 18 57)',
+                fontSize: 20,
+                fontWeight: 700,
+                position: 'top',
+              }}
+              dataKey='female'
+              fill='rgb(244 114 182)'
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
     </div>
   )
 }
